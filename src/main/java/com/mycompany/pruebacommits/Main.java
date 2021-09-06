c655afb6f7573a5f00a777457148dafbb25fe1ce
ivan vasquez <ivan.vasquez.2020@upb.edu.co>
2021-09-05 19:37:19 -0500

    prueba de apagado

--- src/main/java/com/mycompany/pruebacommits/Main.java
+++ src/main/java/com/mycompany/pruebacommits/Main.java
@@ -12,5 +12,6 @@ package com.mycompany.pruebacommits;
 public class Main {
     public static void main(String[] args) {
         System.out.println("hola mundo");
+        System.out.println("hi men");
     }
 }
